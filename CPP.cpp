3b38def16254a3b6c6709dd4ec88f8d4842d15a8
HJTN <zintn2012@gmail.com>
2021-08-04 23:33:02 +0900

    Modify : CPP(Class, Constructor)

--- CPP.cpp
+++ CPP.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
-#include <list>
 using namespace std;
 
-int main()
-{
-    list<int> ls = { 10, 20, 30 };
-    ls.push_back(100);
-    ls.push_front(3);
+class Book {
+    private:
+        void set_percent();
+    public:
+        Book();
+        Book(string title, int total_page);
+        string title;
+        int currentPage;
+        int totalPage;
+        void Move(int page);
+};
 
-    list<int>::iterator iter = ls.begin();
-    *iter = 5;      // list[0] = 5;
+int main() {
+    Book webBook("CPP 생성자", 350);
 
-    for (iter = ls.begin(); iter != ls.end(); iter++)
-        cout << *iter << " ";
+    webBook.Move(30);
+    cout << webBook.currentPage << endl;
+    cout << webBook.title << " " << webBook.totalPage << endl;
+}
+
+// 멤버 함수 정의 == 일반 함수 정의
+void Book::Move(int page) {
+    currentPage = page;
+}
+
+Book::Book(string title, int total_page) {
+    this->title = title;
+    this->totalPage = total_page;
 }
