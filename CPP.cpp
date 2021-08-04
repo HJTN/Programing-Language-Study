e6dbca1b331ebcda572d2784b74400b0b309eb4e
HJTN <zintn2012@gmail.com>
2021-08-05 00:01:07 +0900

    Modify : Change Korean to English

--- CPP.cpp
+++ CPP.cpp
@@ -9,11 +9,11 @@ using namespace std;
 // 벡터 Main
 int main () {
     vector<int> vI = { 10, 20, 30 };
-    cout << "현재 벡터의 크기 : " << vI.size() << endl;
+    cout << "Current vector size : " << vI.size() << endl;
 
     vI.push_back(40);
     // iterator 사용, 벡터의 요소 뽑아 냄
-    cout << "현재 벡터의 크기 : " << vI.size() << endl << "현재 벡터의 요소 : ";
+    cout << "Current vector size : " << vI.size() << endl << "Current vector elements : ";
     for (vector<int>::iterator it = vI.begin(); it != vI.end(); it++)
         cout << *it << " ";
     cout << endl;
@@ -28,20 +28,20 @@ int main() {
     dq.push_back(30);
     dq.push_front(10);
 
-    cout << "현재 데큐의 모든 요소 : ";
+    cout << "Current deque elements : ";
     for (deque<int>::iterator it = dq.begin(); it != dq.end(); it++)
         cout << *it << " ";
     cout << endl;
 
     dq.pop_back();
     dq.pop_front();
-    cout << "현재 데큐의 모든 요소 : ";
+    cout << "Current deque elements : ";
     for (deque<int>::iterator it = dq.begin(); it != dq.end(); it++)
         cout << *it << " ";
     cout << endl;
 
     dq.at(0) = 10;
-    cout << "at 사용" << dq[0] << endl;
+    cout << "Use at method" << dq[0] << endl;
 }
 
 // Forward_list Main
@@ -50,12 +50,12 @@ int main() {
     forward_list<int> fls02 = { 40, 50 };
     forward_list<int>::iterator it;
 
-    cout << "현재 순방향 리스트 fls01의 모든 요소 : ";
+    cout << "Current forward_list fls01 elements : ";
     for (it = fls01.begin(); it != fls01.end(); it++)
         cout << *it << " ";
     cout << endl;
 
-    cout << "현재 순방향 리스트 fls02의 모든 요소 : ";
+    cout << "Current forward_list fls02 elements : ";
     for (it = fls02.begin(); it != fls02.end(); it++)
         cout << *it << " ";
     cout << endl;
@@ -63,12 +63,12 @@ int main() {
     it = fls01.begin();
     fls01.splice_after(it, fls02);
 
-    cout << "현재 순방향 리스트 fls01의 모든 요소 : ";
+    cout << "Current forward_list fls01 elements : ";
     for (it = fls01.begin(); it != fls01.end(); it++)
         cout << *it << " ";
     cout << endl;
 
-    cout << "현재 순방향 리스트 fls02의 모든 요소 : ";
+    cout << "Current forward_list fls02 elements : ";
     for (it = fls02.begin(); it != fls02.end(); it++)
         cout << *it << " ";
     cout << endl;
