0076a0f534d262c7f1adb3842cb543d3418a83ba
HJTN <zintn2012@gmail.com>
2021-08-05 00:23:21 +0900

    Modify : language

--- CPP.cpp
+++ CPP.cpp
@@ -10,14 +10,14 @@ int main() {
     set<int> st(arr, arr + 3);
     set<int>::iterator it;
 
-    cout << "현재 집합의 모든 요소 : ";
+    cout << "Current set elements : ";
     for (it = st.begin(); it != st.end(); it++)
         cout << *it << " ";
 
     st.insert(60);
     st.insert(70);
     st.erase(30);
-    cout << endl << "현재 집합의 모든 요소 : ";
+    cout << endl << "Current set elements : ";
     copy(st.begin(), st.end(), ostream_iterator<int>(cout, " "));
 }
 /*
@@ -25,10 +25,10 @@ int main() {
 int main() {
     map<string, int> mp;
     map<string, int>::iterator it;
-    mp.insert(pair<string, int>("국어", 80));
-    mp["수학"] = 100;
+    mp.insert(pair<string, int>("math", 80));
+    mp["english"] = 100;
 
-    cout << "현재 맵의 모든 요소 : ";
+    cout << "Current map elements : ";
     for (it = mp.begin(); it != mp.end(); it++)
         cout << it->first << " : " << it->second << endl;
 }
