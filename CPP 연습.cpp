d8502828fb2ecf90f4d383d377e5e7156a78bba1
HJTN <zintn2012@gmail.com>
2021-08-04 13:39:06 +0900

    Modify : CPP & Add : CPP.exe

--- CPP 연습.cpp
+++
@@ -1,6 +0,0 @@
-int equal(treePointer first, treePointer second) {
-    return ((!first && !second) || (first && second &&
-            (first->data == second->data) &&
-            equal(first->leftChild,second->leftChild) &&
-            equal(first->rightChild,second->rightChild)))
-}
