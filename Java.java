aaec98dd779e3e9f4d4349582ff9298f5ab55a4b
HJTN <zintn2012@gmail.com>
2021-08-05 19:40:18 +0900

    Modify : Java(init)

---
+++ Java.java
@@ -0,0 +1,15 @@
+class Test {
+    int field;
+    String field2;
+
+    public void method1() {
+        System.out.println("Java practicing file");
+    }
+}
+
+public class Java {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.method1();
+    }
+}
