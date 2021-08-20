cf04a47332996621eb2d57485b4deb93c0fcf7ce
HJTN <zintn2012@gmail.com>
2021-08-20 19:37:05 +0900

    TS : constructor, access modifiers, getter & setter Study

--- TS/app.js
+++ TS/app.js
@@ -1,11 +1,28 @@
 var Employee = /** @class */ (function () {
-    function Employee() {
+    function Employee(_fullName, _age, _jobTitle, _hourlyRate, workingHoursPerWeek) {
         var _this = this;
+        this._fullName = _fullName;
+        this._age = _age;
+        this._jobTitle = _jobTitle;
+        this._hourlyRate = _hourlyRate;
+        this.workingHoursPerWeek = workingHoursPerWeek;
         this.printEmployeeDetails = function () {
-            console.log(_this.fullName + "\uFFFD\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD " + _this.jobTitle + "\uFFFD\u0330\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\n        " + _this.hourlyRate * _this.workingHoursPerWeek + "\uFFFD\u07B7\uFFFD \uFFFD\u0334\uFFFD.");
+            console.log(_this._fullName + "'s job is " + _this._jobTitle + " and revenue of a week is " + _this._hourlyRate * _this.workingHoursPerWeek + "$");
         };
     }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (value) {
+            this._fullName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-var employee1 = new Employee();
+var employee1 = new Employee('Minsu', 28, 'Junior Developer', 40, 35);
+employee1.fullName = 'Henry';
+console.log(employee1.fullName);
 employee1.printEmployeeDetails();
--- TS/app.ts
+++ TS/app.ts
@@ -1,16 +1,29 @@
 class Employee {
-    fullName: string;
-    age: number;
-    jobTitle: string;
-    hourlyRate: number;
-    workingHoursPerWeek: number;
+
+    constructor(
+        private _fullName: string,
+        private _age: number,
+        private _jobTitle: string,
+        private _hourlyRate: number,
+        public workingHoursPerWeek: number) {
+        
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+
+    set fullName(value: string) {
+        this._fullName = value;
+    }
 
     printEmployeeDetails = (): void => {
-        console.log(`${this.fullName}의 직업은 ${this.jobTitle}이고 일주일의 수입은
-        ${this.hourlyRate * this.workingHoursPerWeek}달러 이다.`);
+        console.log(`${this._fullName}'s job is ${this._jobTitle} and revenue of a week is ${this._hourlyRate * this.workingHoursPerWeek}$`);
     }
 }
 
-let employee1 = new Employee();
+let employee1 = new Employee('Minsu', 28, 'Junior Developer', 40, 35);
+employee1.fullName = 'Henry';
+console.log(employee1.fullName);
 employee1.printEmployeeDetails();
 
