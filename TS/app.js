d786143bc06f6d1a0252db42046e495435aa1392
HJTN <zintn2012@gmail.com>
2021-08-20 18:39:01 +0900

    TS : Class Study

--- TS/app.js
+++ TS/app.js
@@ -1,8 +1,11 @@
-var sendGreeting = function (message, userName) {
-    if (message === void 0) { message = 'Hello'; }
-    if (userName === void 0) { userName = 'there'; }
-    return console.log(message + ", " + userName);
-};
-sendGreeting();
-sendGreeting('Good morning');
-sendGreeting('Good morning', 'JinSu');
+var Employee = /** @class */ (function () {
+    function Employee() {
+        var _this = this;
+        this.printEmployeeDetails = function () {
+            console.log(_this.fullName + "\uFFFD\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD " + _this.jobTitle + "\uFFFD\u0330\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD \uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\uFFFD\n        " + _this.hourlyRate * _this.workingHoursPerWeek + "\uFFFD\u07B7\uFFFD \uFFFD\u0334\uFFFD.");
+        };
+    }
+    return Employee;
+}());
+var employee1 = new Employee();
+employee1.printEmployeeDetails();
--- TS/app.ts
+++ TS/app.ts
@@ -1,5 +1,16 @@
-const sendGreeting = (message = 'Hello', userName = 'there'): void => console.log(`${message}, ${userName}`);
+class Employee {
+    fullName: string;
+    age: number;
+    jobTitle: string;
+    hourlyRate: number;
+    workingHoursPerWeek: number;
 
-sendGreeting();
-sendGreeting('Good morning');
-sendGreeting('Good morning', 'JinSu');
+    printEmployeeDetails = (): void => {
+        console.log(`${this.fullName}의 직업은 ${this.jobTitle}이고 일주일의 수입은
+        ${this.hourlyRate * this.workingHoursPerWeek}달러 이다.`);
+    }
+}
+
+let employee1 = new Employee();
+employee1.printEmployeeDetails();
+
