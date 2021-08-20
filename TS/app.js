3ea4f75504a609b5e4ebf2aeef457177cc39754b
HJTN <zintn2012@gmail.com>
2021-08-20 17:31:57 +0900

    TS : Any, Union Type, Type Aliases, Type Guards Study

--- TS/app.js
+++ TS/app.js
@@ -1,27 +1,10 @@
-var studentID = 12345;
-var studentName = 'HeoJinSu';
-var age = 21;
-var gender = 'male';
-var subject = 'C++, Java, etc';
-var courseCompleted = false;
-var student1 = {
-    studentID: 121212,
-    studentName: 'Jack',
-    age: 28,
-    gender: 'male',
-    subject: 'Mongo DB',
-    courseCompleted: false
+var itemPrice;
+var setItemPrice = function (price) {
+    if (typeof price === 'string') { // price Ÿ���� string�� ���
+        itemPrice = 0;
+    }
+    else {
+        itemPrice = price;
+    }
 };
-;
-function getStudentDetails(studentID) {
-    return {
-        studentID: 12345,
-        studentName: 'HeoJinSu',
-        gender: 'male',
-        subject: 'C++, Java, etc',
-        courseCompleted: false
-    };
-}
-function saveStudentDetails(student) {
-}
-saveStudentDetails(student1);
+setItemPrice(50);
--- TS/app.ts
+++ TS/app.ts
@@ -1,43 +1,14 @@
-let studentID: number = 12345;
-let studentName: string = 'HeoJinSu';
-let age: number = 21;
-let gender: string = 'male';
-let subject: string = 'C++, Java, etc';
-let courseCompleted: boolean = false;
+type StringOrNum = string | number;
+let itemPrice: number;
 
-let student1: student = {
-    studentID: 121212,
-    studentName: 'Jack',
-    age: 28,
-    gender: 'male',
-    subject: 'Mongo DB',
-    courseCompleted: false
+const setItemPrice = (price: StringOrNum): void => {
+    if (typeof price === 'string') {    // price 타입이 string일 경우
+        itemPrice = 0;
+    }
+    else {
+        itemPrice = price;
+    }
+    
 };
 
-interface student {
-    readonly studentID: number,
-    studentName: string,
-    age?: number,
-    gender: 'male' | 'female' | 'genderNeutral',        // Literal type
-    subject: string,
-    courseCompleted: boolean,
-    addComment?(comment: string): string,
-    addComment?: (Comment: string) => string;
-};
-
-function getStudentDetails(studentID: number): student
-{
-    return {
-        studentID: 12345,
-        studentName: 'HeoJinSu',
-        gender: 'male',
-        subject: 'C++, Java, etc',
-        courseCompleted: false
-    };
-}
-
-function saveStudentDetails(student: student): void {
-
-}
-
-saveStudentDetails(student1);
+setItemPrice(50);
